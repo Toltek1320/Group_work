fc98409429b76395bfc542b5e1c3d5dc476f2d6f
Ruslan <123066002+Toltek1320@users.noreply.github.com>
2023-05-23 15:58:37 +0300

    Update model.py

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ class PhoneBook:
     def __init__(self, filename):
         self._filename = filename
 
-    def search_contact(self, name): # Найти контакт по фамилии
+    def search_contact(self, name):                                 # Найти контакт по фамилии
         with open(self._filename, 'r', encoding='utf-8') as file:
             for line in file.readlines():
                 surname, phone_number = line.strip().split(' ')
@@ -17,7 +17,7 @@ class PhoneBook:
                     return phone_number
         return None
 
-    def add_contact(self, name, phone_number): # Добавить контакт
+    def add_contact(self, name, phone_number):                      # Добавить контакт
         with open(self._filename, 'a', encoding='utf-8') as file:
             file.write(f"{name} {phone_number}\n")
 
@@ -30,7 +30,7 @@ class PhoneBook:
                 if surname != name:
                     file.write(line)
 
-    def change_contact(self, name, new_phone_number): # Изменить контакт
+    def change_contact(self, name, new_phone_number):               # Изменить контакт
         with open(self._filename, 'r', encoding='utf-8') as file:
             lines = file.readlines()
         with open(self._filename, 'w', encoding='utf-8') as file:
@@ -41,6 +41,6 @@ class PhoneBook:
                 else:
                     file.write(line)
 
-    def get_contacts(self): # Посмотреть все контакты
+    def get_contacts(self):                                         # Посмотреть все контакты
         with open(self._filename, 'r', encoding='utf-8') as file:
             return file.read().strip().split('\n')
